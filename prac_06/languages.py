d0fae00d823047738b7313ad3f2b45f280623c06
saung <your.email@example.com>
2025-03-11 23:33:15 +0800

    Add script to create and filter ProgrammingLanguage objects

---
+++ prac_06/languages.py
@@ -0,0 +1,15 @@
+from programming_langugage import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+print(python)
+
+languages = [python, ruby, visual_basic]
+
+dynamic_languages = [language.name for language in languages if language.is_dynamic()]
+
+print("The dynamically typed languages are:")
+for language in dynamic_languages:
+    print(language)
