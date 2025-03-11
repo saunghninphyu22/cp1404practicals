19618f3be4b5cf5b7e70332c7d1482c88df41ecf
saung <your.email@example.com>
2025-03-12 00:51:39 +0800

    Add ProgrammingLanguage class with attributes

--- prac_06/programming_langugage.py
+++ prac_06/programming_langugage.py
@@ -4,6 +4,7 @@ Actual: 12 minutes
 """
 
 class ProgrammingLanguage:
+    """ A Programming Language class with a name, type, reflection and year."""
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
@@ -11,7 +12,9 @@ class ProgrammingLanguage:
         self.year = year
 
     def is_dynamic(self):
+        """Check if the programming language is dynamic."""
         return self.typing.lower() == "dynamic"
 
     def __str__(self):
+        """Return a string representation of the programming language."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
